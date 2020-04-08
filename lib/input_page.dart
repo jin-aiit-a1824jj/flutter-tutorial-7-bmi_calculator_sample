5962541d6fe451aa24e086bbb6fd14daa07c46e9
a1824jj <a1824jj@aiit.ac.jp>
2020-04-08 13:02:32 +0900

    refactor flutter widgets

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,20 +9,54 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR SAMPEL'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: Theme(
-        data: ThemeData(
-            accentColor: Colors.purple
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR SAMPEL'),
         ),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
-      ),
-    );
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(children: <Widget>[
+                Expanded(
+                    child: ReuseAbleCard(color: Color(0xFF1D1E33))
+                ),
+                Expanded(
+                    child: ReuseAbleCard(color: Color(0xFF1D1E33))
+                ),
+              ]),
+            ),
+            Expanded(
+              child: ReuseAbleCard(color: Color(0xFF1D1E33)),
+            ),
+            Expanded(
+              child: Row(children: <Widget>[
+                Expanded(
+                    child: ReuseAbleCard(color: Color(0xFF1D1E33))
+                ),
+                Expanded(
+                    child: ReuseAbleCard(color: Color(0xFF1D1E33))
+                ),
+              ]),
+            ),
+          ],
+        ));
   }
+}
+
+class ReuseAbleCard extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+                      decoration: BoxDecoration(
+                          color: this.color,
+                          borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      margin: EdgeInsets.all(15.0),
+                );
+  }
+
+  final Color color;
+  ReuseAbleCard({@required this.color});
+
 }
